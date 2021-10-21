985bacc1bc83e9cf25c988a68a8c884d34a7a4f5
Norbert Olesiak-Zbieg <n.olesiak@gmail.com>
2021-10-21 10:35:41 +0200

    repository set

---
+++ CleanArchitectureWorkshop.Application/Contracts/Persistance/IAsyncRepository.cs
@@ -0,0 +1,12 @@
+﻿using DevExpress.Xpo;
+using System;
+
+namespace CleanArchitectureWorkshop.Application.Contracts.Persistance
+{
+    public interface IAsyncRepository<T> where T : class
+    {
+      
+        //GetById
+    }
+
+}
---
+++ CleanArchitectureWorkshop.Application/Contracts/Persistance/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureWorkshop.Application.Contracts.Persistance
+{
+    public interface IOrderRepository<T> where T : class
+    {
+    }
+}
