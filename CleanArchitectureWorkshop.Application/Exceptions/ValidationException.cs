46977dd598686da48208ce65b4161ee1ca6f5185
Norbert Olesiak-Zbieg <n.olesiak@gmail.com>
2021-10-21 11:49:43 +0200

    pt3

---
+++ CleanArchitectureWorkshop.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureWorkshop.Application.Exceptions
+{
+    public class ValidationException : ApplicationException
+    {
+        public List<string> ValidationErrors { get; set; }
+
+    }
+}
---
+++ CleanArchitectureWorkshop.Application/Features/Events/Commands/CreateEvent/CreateEventCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureWorkshop.Application.Features.Events.Commands.CreateEvent
+{
+    public class CreateEventCommandValidator : AbstractValidator<CreateEventCommand>
+    {
+        public CreateEventCommandValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotNull();
+                
+        }
+
+    }
+}
